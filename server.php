d05ae5d266451fd81f495b9cc98ceaa334d922d6
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-09-26 15:00:18 +0200

    edit

--- server.php
+++ server.php
@@ -1,26 +1,33 @@
 <?php
 
 $word = $_GET ["word"];
+$paragraph = $_GET ["paragraph"];
 
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
 </head>
+
 <body>
   <p>
-  La parola che hai scritto è: <?php echo $word ?>
+    La parola che hai scritto è: <?php echo $word ?>
   </p>
   <p>
     La parola è lunga: <?php  echo strlen($word) ?> lettere
   </p>
   <p>
-    Parola censurata: <?php echo str_replace('a','***',$word) ?>
+    Parola censurata: <?php echo str_replace('ciao','***',$paragraph) ?>
+  </p>
+  <p>
+    Parola censurata: <?php echo strlen(str_replace('lorem','***',$paragraph))  ?>
   </p>
 </body>
-</html> 
+
+</html>
